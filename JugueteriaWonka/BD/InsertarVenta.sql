8961e100e2f40cafe996e08318c578d7bdc357aa
Baltazar9526 <32987728+Baltazar9526@users.noreply.github.com>
2017-10-24 08:10:59 -0500

    Update InsertarVenta.sql

--- JugueteriaWonka/BD/InsertarVenta.sql
+++ JugueteriaWonka/BD/InsertarVenta.sql
@@ -1,30 +1,38 @@
 USE [JugueteriaWonka]
 GO
-
-/****** Object:  StoredProcedure [dbo].[pa_insVenta]    Script Date: 22-Oct-17 2:12:06 PM ******/
+/****** Object:  StoredProcedure [dbo].[pa_insVenta]    Script Date: 24-Oct-17 8:11:05 AM ******/
 SET ANSI_NULLS ON
 GO
-
 SET QUOTED_IDENTIFIER ON
 GO
-
-CREATE PROCEDURE [dbo].[pa_insVenta]
-	@Descipcion VARCHAR(180),
-	@IdFomaPago INT OUT,
+ALTER PROCEDURE [dbo].[pa_insVenta]
 	@Fecha VARCHAR(20),
 	@IdEmpleado INT,
 	@IdCliente INT, 
-	@IdVenta INT OUT,
-	@CantidadProducto INT,
-	@Precio INT,
-	@IdProducto INT
+	@IdFomaPago INT,
+	@IdVenta INT OUT
 AS
 BEGIN
-	INSERT INTO FormaPago (Descripcion) VALUES (@Descipcion);
-	SET @IdFomaPago = SCOPE_IDENTITY();
+	SET NOCOUNT ON;
+	BEGIN TRANSACTION
+	BEGIN TRY
 	INSERT INTO Venta (Fecha, IdEmpleado, IdCliente, IdFormaPago) VALUES (CONVERT(DATE, @Fecha, 103), @IdEmpleado, @IdCliente, @IdFomaPago);
 	SET @IdVenta = SCOPE_IDENTITY();
-	INSERT INTO DetalleVenta (CantidadProducto, Precio, IdVenta, IdProducto) VALUES (@CantidadProducto, @Precio, @IdVenta, @IdProducto);
+	END TRY
+	BEGIN CATCH
+		declare @errnumber int=0, 
+		@errseverity int=0, 
+		@errstate int=0,
+		@errproc nvarchar(126)=N'',
+		@errline int=0,
+		@errmsg nvarchar(2048)=N''
+		select	@errnumber=ERROR_NUMBER(),
+				@errseverity=ERROR_SEVERITY(),
+				@errstate=ERROR_STATE(),
+				@errproc=ERROR_PROCEDURE(),
+				@errline=ERROR_LINE(),
+				@errmsg=ERROR_MESSAGE()
+		select ERROR_NUMBER(),ERROR_SEVERITY(),ERROR_STATE(),ERROR_PROCEDURE(),ERROR_LINE(),ERROR_MESSAGE()
+		ROLLBACK TRANSACTION
+	END CATCH
 END
-GO
-
