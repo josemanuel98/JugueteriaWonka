85b8c67d63e261814a942c9bac9f0f50332fc831
Baltazar9526 <32987728+Baltazar9526@users.noreply.github.com>
2017-10-24 08:00:34 -0500

    Update ActualizarCliente.sql

--- JugueteriaWonka/BD/ActualizarCliente.sql
+++ JugueteriaWonka/BD/ActualizarCliente.sql
@@ -1,42 +1,30 @@
-CREATE PROCEDURE uspActualizarCliente
-@Nombre VARCHAR(50),
-@Paterno VARCHAR(50),
-@Materno VARCHAR(50),
-@Genero VARCHAR(2),
-@Rfc VARCHAR(14),
-@Curp VARCHAR(20),
-@FechaNacimiento VARCHAR(20),
-@Cp VARCHAR(15),
-@Fotografia TEXT,
-@Domicilio VARCHAR(128),
-@Email VARCHAR(128),
-@Telefono VARCHAR(20),
-@IdPersona INT,
-@IdCliente INT,
-@Activo INT
+USE [JugueteriaWonka]
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+ALTER PROCEDURE [dbo].[pa_actCliente] 
+	@id int,
+	@Nombre VARCHAR(50),
+	@ApellidoPaterno VARCHAR(50),
+	@ApellidoMaterno VARCHAR(50),
+	@Genero VARCHAR(2),
+	@Rfc VARCHAR(14),
+	@Curp VARCHAR(20),
+	@FechaNacimiento varchar(20), --DD/MM/AAAA
+	@Cp VARCHAR(15),
+	@Fotografia TEXT,--Se guardará la Imagen como texto.			
+	@Domicilio VARCHAR(128),
+	@Email VARCHAR(128),
+	@Telefono VARCHAR(20)
 AS
 BEGIN
 	SET NOCOUNT ON
 	BEGIN TRANSACTION
 	BEGIN TRY
-		UPDATE Persona
-			SET Nombre = @Nombre,
-				ApellidoPaterno = @Paterno,
-				ApellidoMaterno = @Materno,
-				Genero = @Genero,
-				Rfc = @Rfc,
-				Curp = @Curp,
-				FechaNacimiento = @FechaNacimiento,
-				Cp = @Cp,
-				Fotografia = @Fotografia,
-				Domicilio = @Domicilio
-			WHERE IdPersona = @IdPersona
-		UPDATE Cliente
-			SET Email = @Email,
-				Telefono = @Telefono,
-				Activo = @Activo
-			WHERE IdCliente = @IdCliente
-		COMMIT TRANSACTION
+	UPDATE Persona SET Nombre=@Nombre, ApellidoPaterno=@ApellidoPaterno, ApellidoMaterno=@ApellidoMaterno,Genero=@Genero, Rfc=@Rfc, Curp=@Curp, FechaNacimiento=CONVERT(DATE, @FechaNacimiento, 103), Cp=@Cp,Fotografia=@Fotografia, Domicilio=@Domicilio WHERE IdPersona=@id
+	UPDATE Cliente SET Email=@Email, Telefono=@Telefono WHERE IdPersona=@id
 	END TRY
 	BEGIN CATCH
 		declare @errnumber int=0, 
@@ -45,14 +33,12 @@ BEGIN
 		@errproc nvarchar(126)=N'',
 		@errline int=0,
 		@errmsg nvarchar(2048)=N''
-
-		select @errnumber=ERROR_NUMBER(),
-			@errseverity=ERROR_SEVERITY(),
-			@errstate=ERROR_STATE(),
-			@errproc=ERROR_PROCEDURE(),
-			@errline=ERROR_LINE(),
-			@errmsg=ERROR_MESSAGE()
-
+		select	@errnumber=ERROR_NUMBER(),
+				@errseverity=ERROR_SEVERITY(),
+				@errstate=ERROR_STATE(),
+				@errproc=ERROR_PROCEDURE(),
+				@errline=ERROR_LINE(),
+				@errmsg=ERROR_MESSAGE()
 		select ERROR_NUMBER(),ERROR_SEVERITY(),ERROR_STATE(),ERROR_PROCEDURE(),ERROR_LINE(),ERROR_MESSAGE()
 		ROLLBACK TRANSACTION
 	END CATCH
