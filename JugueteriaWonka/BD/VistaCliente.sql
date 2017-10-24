95c6facd31581083aae8afc088a33b96a03650be
Baltazar9526 <32987728+Baltazar9526@users.noreply.github.com>
2017-10-24 07:56:04 -0500

    Update VistaCliente.sql

--- JugueteriaWonka/BD/VistaCliente.sql
+++ JugueteriaWonka/BD/VistaCliente.sql
@@ -1,18 +1,26 @@
-CREATE VIEW V_Cliente
-AS
-SELECT Cliente.IdCliente,
-		Persona.IdPersona,
-		Persona.Nombre,
-		Persona.ApellidoPaterno,
-		Persona.ApellidoMaterno,
-		Persona.Genero,
-		Persona.Rfc,
-		Persona.Curp,
-		CONVERT(NVARCHAR, Persona.FechaNacimiento, 103) AS FechaNacimientol,
-		Persona.Cp,
-		Persona.Fotografia,
-		Persona.Domicilio,
-		Cliente.Email,
-		Cliente.Telefono,
-		Cliente.Activo
-		FROM Cliente INNER JOIN Persona ON Cliente.IdPersona = Persona.IdPersona
+USE [JugueteriaWonka]
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE view [dbo].[V_Cliente] as
+	select C.IdCliente,
+		   P.IdPersona,
+		   P.Nombre,
+		   P.ApellidoPaterno,
+		   P.ApellidoMaterno,
+		   P.Genero,
+		   P.Rfc,
+		   P.Curp,
+		   CONVERT(nvarchar, P.FechaNacimiento, 103) as FechaNacimiento,
+		   P.Cp,
+		   P.Fotografia,
+		   P.Domicilio,
+		   C.Email,
+		   C.Telefono,
+		   C.Activo
+	from   Cliente C INNER JOIN Persona P on C.IdPersona = P.IdPersona
+	WHERE C.Activo > 0;
+
+GO
